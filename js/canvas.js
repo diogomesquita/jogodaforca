82093ad1cc7b25f6386be01cde4e969aa348c40e
Diogo Mesquita <doc.diogomesquita@gmail.com>
2023-02-12 07:02:49 -0300

    initial menu done

--- js/canvas.js
+++ js/canvas.js
@@ -17,8 +17,8 @@ function desenharLinhas() {
     jogo.lineWidth = 6;
     jogo.lineCap = "round";
     jogo.lineJoin = "round";    
-    jogo.fillStyle = "#F3F5FC"
-    jogo.strokeStyle = "#0A3871"
+    jogo.fillStyle = "#F3F5FC";
+    jogo.strokeStyle = "#0A3871";
 
     let tracinhosPalavra = 600/segredo.length;
     for(let i = 0; i < segredo.length; i++) {
@@ -27,4 +27,21 @@ function desenharLinhas() {
     }
     jogo.stroke();
     jogo.closePath();
+}
+
+function escreverAcerto(index) {
+    jogo.font = "bold 42px Inter";
+    jogo.lineCap = "round";
+    jogo.fillStyle = "#0A3871";
+    jogo.lineWidth = 6;
+    let tracinhosPalavra = 600/segredo.length;
+    jogo.fillText(segredo[index], 505+(tracinhosPalavra * index), 620);
+}
+
+function escreverErro(letra, erros) {
+    jogo.font = "bold 36px Inter";
+    jogo.lineCap = "round";
+    jogo.fillStyle = "#0A3871";
+    jogo.lineWidth = 6;
+    jogo.fillText(letra, 520 + (40 * (10 -erros)), 700, 36);
 }
--- js/forca.js
+++ js/forca.js
@@ -1,15 +1,62 @@
-let segredos = ["Deus", "vida", "familia", "gratidao", "sucesso", "liberdade", "paz", "respeito", "objetivo", "escolhas"];
-let segredo = "";
+let addPalavra = document.querySelector("#addPalavra");
 let jogo = document.querySelector("#forca").getContext('2d');
+let segredos = ["DEUS", "VIDA", "FAMILIA", "GRATIDAO", "SUCESSO", "LIBERDADE", "PAZ", "RESPEITO", "OBJETIVO", "ESCOLHAS"];
+let segredo = "";
+let letras = [];
+let gameOver = 8;
+
+addPalavra.addEventListener("keydown", (e) => {
+    if(e.keyCode === 13) {
+        segredos.push(addPalavra.value.toUpperCase());
+        console.log(segredos);
+    }
+});
 
 function sortearPalavra() {
     let palavra = segredos[Math.floor(Math.random() * segredos.length)];
     segredo = palavra;
 }
 
+function verificarLetra(key) {
+    let estado = false;
+    if(key >= 65 && letras.indexOf(key) || key <= 90 && letras.indexOf(key)) {
+        letras.push(key);
+        console.log(key);
+        console.log(letras);
+        return estado;
+    } else {
+        estado = true;
+        letras.push(key);
+        console.log(key, "do true");
+        console.log(letras, "do true");
+        return estado;
+    }
+}
+
+function contarErros() {
+    gameOver -= 1;
+    console.log(gameOver);
+}
+
 function iniciarJogo() {
     document.querySelector(".menuInicial").style.display = "none";
     sortearPalavra();
+
     desenharCanvas();
     desenharLinhas();
+
+    document.onkeydown = (e) => {
+        let letra = e.key.toUpperCase();
+
+        if(verificarLetra(letra) && segredo.includes(letra)) {
+            for(let i = 0; i < segredo.length; i++) {
+                if(segredo[i] === letra){
+                    escreverAcerto(i);
+                }
+            }
+        } else {
+            contarErros(letra);
+            escreverErro(letra, gameOver);
+        }
+    }
 }
